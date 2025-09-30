33cb908196afd10e322bdaf4f83777b3b00f3ac6
Lucas Sarasa <lucasmsarasa@gmail.com>
2025-09-30 14:45:48 -0300

    feat: add orders page

--- src/app/[slug]/menu/actions/create-order.ts
+++ src/app/[slug]/menu/actions/create-order.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { ConsumptionMethod } from "@prisma/client";
+import { redirect } from "next/navigation";
 
 import { db } from "@/lib/prisma";
 
@@ -58,4 +59,6 @@ export const createOrder = async (input: CreateOrderInput) => {
       restaurantsId: restaurant.id,
     },
   });
+
+  redirect(`/${input.slug}/orders`);
 };
