3eef71f20edfaeb6548c4b5708c0e40ce134e463
Lucas Sarasa <lucasmsarasa@gmail.com>
2025-09-30 16:59:03 -0300

    feat: ensure cache is revalidated when creating an order

--- src/app/[slug]/menu/actions/create-order.ts
+++ src/app/[slug]/menu/actions/create-order.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { ConsumptionMethod } from "@prisma/client";
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 import { db } from "@/lib/prisma";
@@ -59,7 +60,7 @@ export const createOrder = async (input: CreateOrderInput) => {
       restaurantsId: restaurant.id,
     },
   });
-
+  revalidatePath(`/${input.slug}/orders`)
   redirect(
     `/${input.slug}/orders?cpf=${removeCpfPunctuation(input.customerCpf)}`,
   );
