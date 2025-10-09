9d176d27f157ba99047a78f7bd39ab40c9776963
Lucas Sarasa <lucasmsarasa@gmail.com>
2025-10-09 17:01:31 -0300

    chore: update favicon for the app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   title: "FSW Donalds",
   description: "Application to find restaurants nearby you",
+  icons: {
+    icon: "/fastfood-icon.svg",
+  },
 };
 
 export default function RootLayout({
@@ -27,7 +30,7 @@ export default function RootLayout({
       <body className={`${poppins.className} antialiased`}>
         <CartProvider>{children}</CartProvider>
 
-        <Toaster/>
+        <Toaster />
       </body>
     </html>
   );
